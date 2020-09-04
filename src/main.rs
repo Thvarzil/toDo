1df22cfea050c0e14c6ddc7a759d557587742d06
Thvarzil <louisblebohec@gmail.com>
2020-09-04 12:39:15 -0400

    added sample file, and file read functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+//use std::io::BufferedReader;
+//use std::io::File;
+//use std::from_str::from_str;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let fname = "list.txt";
+    //let path = Path::new(fname);
+    //let mut file = BufferedReader::new(File::open(&path));
+    
+    //read contents of file
+    let contents = fs::read_to_string(fname)
+        .expect("Something went wrong reading the file");
+    //separate contents into separate lines
+    let items = contents.lines().collect::<Vec<_>>();
+
+    //iterate through lines and print them
+    for item in items.iter()
+    {
+        println!("Task: {}", item);
+    }
+
 }
