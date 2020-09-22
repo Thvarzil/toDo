ee9fd063e5415765edd2e1ccf05d4317c3bc7764
Thvarzil <louisblebohec@gmail.com>
2020-09-22 16:01:13 -0600

    Application now successfully rewites .txt file with added content

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,29 @@ use std::io::Write;
 
 
 fn main() {
-    let fname = "list.txt";
-    
+    let fname = "./src/list.txt";
+
+    let mut file = File::open(fname).unwrap();
+
     //read contents of file
     let contents = fs::read_to_string(fname)
         .expect("Something went wrong reading the file");
+
     //separate contents into separate lines
     let mut items = contents.lines().collect::<Vec<_>>();
+
+  
+
+    //Test adding items to list
+    items.push("Replace .txt file with database?");
+
+    let mut file_save = String::new();
+
+    for item in items.iter()
+    {
+        file_save += item;
+        file_save += "\n"
+    }
 
     //iterate through lines and print them
     for (i, item) in items.iter().enumerate()
@@ -18,20 +34,27 @@ fn main() {
         println!("Task {}: {}", i+1, item);
     }
 
-    //Test adding items to list
-    items.push("Take a nap in R'lyeh");
+    println!("{}",file_save);
 
-  
-
-    let mut file = File::open(fname).unwrap();
     
+    //This fragment replaces the file with a file containing "Test write - replace"
+    let mut fileNew = File::create(fname).unwrap();
+    fileNew.write_all(file_save.as_bytes());
 
+    /*
+    
+    
     for item in items.iter()
     {
         file.write_all(item.as_bytes());
      
     }
+    */
 
-    file.sync_all();
+    
+    
+    
+
+    
   
 }
